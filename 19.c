bb6ed909246be2be44fadf42cb9fe797a78d27cb
Parranoh <minerva.schlau@hotmail.com>
2023-09-30 06:20:00 +0200

    day 19

--- 19.c
+++ 19.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
 
 #define MAX_BLUEPRINTS 32
-#define TIME 24
+#define TIME_1 24
+#define TIME_2 32
+#define N_BLUEPRINTS_2 3
 
 static struct blueprint {
     int id;
@@ -30,38 +33,24 @@ void input(void)
             ++n_blueprints);
 }
 
-static int div_ceil(int x, int y)
-{ return (x + y - 1) / y; }
-
 struct state {
     int ore, clay, obsidian, geode;
     int ore_robots, clay_robots, obsidian_robots, geode_robots;
 };
 static const struct state init_state = {0, 0, 0, 0, 1, 0, 0, 0};
 
-static int t_ore(const struct blueprint *bp, const struct state *s)
-{ return div_ceil(bp->ore_ore - s->ore, s->ore_robots); }
-
-static int t_clay(const struct blueprint *bp, const struct state *s)
-{ return div_ceil(bp->clay_ore - s->ore, s->ore_robots); }
-
-static int t_obsidian(const struct blueprint *bp, const struct state *s)
-{
-    if (s->clay_robots <= 0)
-        return -1;
-    const int t_obsidian_ore = div_ceil(bp->obsidian_ore - s->ore, s->ore_robots);
-    const int t_obsidian_clay = div_ceil(bp->obsidian_clay - s->clay, s->clay_robots);
-    return MAX(t_obsidian_ore, t_obsidian_clay);
-}
-
-static int t_geode(const struct blueprint *bp, const struct state *s)
-{
-    if (s->obsidian_robots <= 0)
-        return -1;
-    const int t_geode_ore = div_ceil(bp->geode_ore - s->ore, s->ore_robots);
-    const int t_geode_obsidian = div_ceil(bp->geode_obsidian - s->obsidian, s->obsidian_robots);
-    return MAX(t_geode_ore, t_geode_obsidian);
-}
+static char can_buy_ore_robot(const struct blueprint *bp, const struct state *s)
+{ return s->ore >= bp->ore_ore; }
+static char can_buy_clay_robot(const struct blueprint *bp, const struct state *s)
+{ return s->ore >= bp->clay_ore; }
+static char can_buy_obsidian_robot(const struct blueprint *bp, const struct state *s)
+{ return s->ore >= bp->obsidian_ore && s->clay >= bp->obsidian_clay; }
+static char can_buy_geode_robot(const struct blueprint *bp, const struct state *s)
+{ return s->ore >= bp->geode_ore && s->obsidian >= bp->geode_obsidian; }
+static int saving_for_obsidian_robot(const struct blueprint *bp, const struct state *s)
+{ return (s->ore < bp->obsidian_ore || (s->clay < bp->obsidian_clay && s->clay_robots > 0)); }
+static int saving_for_geode_robot(const struct blueprint *bp, const struct state *s)
+{ return (s->ore < bp->geode_ore || (s->obsidian < bp->geode_obsidian && s->obsidian_robots > 0)); }
 
 static void buy_ore_robot(const struct blueprint *bp, struct state *s)
 { s->ore -= bp->ore_ore; ++s->ore_robots; }
@@ -72,85 +61,129 @@ static void buy_obsidian_robot(const struct blueprint *bp, struct state *s)
 static void buy_geode_robot(const struct blueprint *bp, struct state *s)
 { s->ore -= bp->geode_ore; s->obsidian -= bp->geode_obsidian; ++s->geode_robots; }
 
-static void unbuy_ore_robot(const struct blueprint *bp, struct state *s)
-{ s->ore += bp->ore_ore; --s->ore_robots; }
-static void unbuy_clay_robot(const struct blueprint *bp, struct state *s)
-{ s->ore += bp->clay_ore; --s->clay_robots; }
-static void unbuy_obsidian_robot(const struct blueprint *bp, struct state *s)
-{ s->ore += bp->obsidian_ore; s->clay += bp->obsidian_clay; --s->obsidian_robots; }
-/* static void unbuy_geode_robot(const struct blueprint *bp, struct state *s) */
-/* { s->ore += bp->geode_ore; s->obsidian += bp->geode_obsidian; --s->geode_robots; } */
-
-int max_geodes_greedy(const struct blueprint *bp)
+static void tick(struct state *s)
 {
-    struct state s = init_state;
-    for (unsigned char t = 0; t < TIME; ++t)
-    {
-        printf("t = %2d", t);
-        struct state s_ = s;
-        s_.ore      += s.ore_robots;
-        s_.clay     += s.clay_robots;
-        s_.obsidian += s.obsidian_robots;
-        s_.geode    += s.geode_robots;
-
-        const int t_g = t_geode(bp, &s);
-        const int t_o = t_obsidian(bp, &s);
-        const int t_c = t_clay(bp, &s);
-        if (t_g == 0)
-        {
-            buy_geode_robot(bp, &s_);
-            printf("\tbuy geode robot");
-        }
-        else if (t_o == 0)
-        {
-            buy_obsidian_robot(bp, &s_);
-            if (t_g >= 0 && t_g <= t_geode(bp, &s_))
-            {
-                unbuy_obsidian_robot(bp, &s_);
-                goto try_clay;
-            }
-            printf("\tbuy obsidian robot");
-        }
-        else try_clay: if (t_c == 0)
-        {
-            buy_clay_robot(bp, &s_);
-            if ((t_g >= 0 && t_g <= t_geode(bp, &s_))
-                    || (t_o >= 0 && t_o <= t_obsidian(bp, &s_)))
-            {
-                unbuy_clay_robot(bp, &s_);
-                goto try_ore;
-            }
-            printf("\tbuy clay robot");
-        }
-        else try_ore: if (t_ore(bp, &s) == 0)
-        {
-            buy_ore_robot(bp, &s_);
-            if ((t_g >= 0 && t_g <= t_geode(bp, &s_))
-                    || (t_o >= 0 && t_o <= t_obsidian(bp, &s_))
-                    || (t_c >= 0 && t_c <= t_clay(bp, &s_)))
-                unbuy_ore_robot(bp, &s_);
-            else
-                printf("\tbuy ore robot");
-        }
-
-        s = s_;
-        printf("\n\t%d robots have collected %d ore\n", s.ore_robots, s.ore);
-        printf("\t%d robots have collected %d clay\n", s.clay_robots, s.clay);
-        printf("\t%d robots have collected %d obsidian\n", s.obsidian_robots, s.obsidian);
-        printf("\t%d robots have collected %d geode\n", s.geode_robots, s.geode);
-    }
-    return s.geode;
+    s->ore += s->ore_robots;
+    s->clay += s->clay_robots;
+    s->obsidian += s->obsidian_robots;
+    s->geode += s->geode_robots;
 }
 
-int part1(void)
+static int max_geodes_brute_force(const struct blueprint *bp, size_t time)
+{
+    int res = 0;
+    int max_ore_cost = MAX(bp->clay_ore, bp->obsidian_ore);
+    max_ore_cost = MAX(max_ore_cost, bp->geode_ore);
+    struct frame {
+        struct state s;
+        enum {
+            nop = 0,
+            ore = 1,
+            clay = 2,
+            obsidian = 3,
+            geode = 4,
+            done = 5
+        } n;
+    } stack[TIME_2];
+    stack[0].s = init_state;
+    stack[0].n = 0;
+    size_t index = 0;
+    while (1)
+    {
+        struct frame *const f = &stack[index];
+        if (index == time - 1)
+        {
+            tick(&f->s);
+            res = MAX(res, f->s.geode);
+            --index;
+            continue;
+        }
+
+        switch (f->n)
+        {
+            case nop:
+                // do not nop unless we are saving for something or time is almost up
+                if (index >= time - 5
+                    || !can_buy_ore_robot(bp, &f->s)
+                    || !can_buy_clay_robot(bp, &f->s)
+                    || saving_for_obsidian_robot(bp, &f->s)
+                    || saving_for_geode_robot(bp, &f->s))
+                {
+                    struct frame *const f_ = &stack[++index];
+                    f_->s = f->s;
+                    tick(&f_->s);
+                    f_->n = 0;
+                }
+                break;
+            case ore:
+                if (f->s.ore_robots < max_ore_cost && can_buy_ore_robot(bp, &f->s) && index < time - 3)
+                {
+                    struct frame *const f_ = &stack[++index];
+                    f_->s = f->s;
+                    tick(&f_->s);
+                    buy_ore_robot(bp, &f_->s);
+                    f_->n = 0;
+                }
+                break;
+            case clay:
+                if (f->s.clay_robots < bp->obsidian_clay && can_buy_clay_robot(bp, &f->s) && f->s.obsidian_robots < bp->geode_obsidian && index < time - 5)
+                {
+                    struct frame *const f_ = &stack[++index];
+                    f_->s = f->s;
+                    tick(&f_->s);
+                    buy_clay_robot(bp, &f_->s);
+                    f_->n = 0;
+                }
+                break;
+            case obsidian:
+                if (f->s.obsidian_robots < bp->geode_obsidian && can_buy_obsidian_robot(bp, &f->s) && index < time - 3)
+                {
+                    struct frame *const f_ = &stack[++index];
+                    f_->s = f->s;
+                    tick(&f_->s);
+                    buy_obsidian_robot(bp, &f_->s);
+                    f_->n = 0;
+                }
+                break;
+            case geode:
+                if (can_buy_geode_robot(bp, &f->s))
+                {
+                    struct frame *const f_ = &stack[++index];
+                    f_->s = f->s;
+                    tick(&f_->s);
+                    buy_geode_robot(bp, &f_->s);
+                    f_->n = 0;
+                }
+                break;
+            case done:
+                if (index == 0)
+                    return res;
+                else
+                    --index;
+        }
+        ++f->n;
+    }
+}
+
+static int part1(void)
 {
     int res = 0;
     for (size_t i = 0; i < n_blueprints; ++i)
     {
         const struct blueprint *const bp = &blueprints[i];
-        const int geodes = max_geodes_greedy(bp);
-        printf("Blueprint %d yields %d geodes.\n", bp->id, geodes);
-        res += bp->id * geodes;
+        res += bp->id * max_geodes_brute_force(bp, TIME_1);
+    }
+    return res;
+}
+
+static int part2(void)
+{
+    int res = 1;
+    for (size_t i = 0; i < MIN(N_BLUEPRINTS_2, n_blueprints); ++i)
+    {
+        const int m = max_geodes_brute_force(&blueprints[i], TIME_2);
+        res *= m;
+        printf(" %d\n", m);
     }
     return res;
 }
@@ -159,5 +192,6 @@ int main(void)
 {
     input();
     printf("%d\n", part1());
+    printf("%d\n", part2());
     return 0;
 }
